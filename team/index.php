81b2d14cd030b695a0203d4cf46a26dd9566fb47
Jack Doan <me@jackdoan.com>
2015-12-02 20:42:31 -0500

    updates

--- team/index.php
+++ team/index.php
@@ -31,26 +31,28 @@ $aaron_bio = "Aaron has competed in various robotics competitions for over 12 ye
 
 //new Person('name', $placeholderPhoto, 'title', 'class', 'email', 'teams', 'bio');
 
-
+$jake = new Person('Jake Rawlins', $placeholderPhoto, 'Laser Operator', '0', 'jake.rawlins@utdallas.edu', 'teams', 'Jake is in charge of all things to do with lasers. He also eats our pizza.');
 $alex = new Person('Alex Koala', $placeholderPhoto, 'Alum', '0', 'email', 'mechanical, alumi', 'Alex did a lot of important things, which we will list here.');
 $stuart = new Person('Stuart Yun', $placeholderPhoto, 'Alum', '0', 'stuart.yun@gmail.com', 'mechanical, alumni', '"No."');
 
 $alejandro = new Person('Alejandro Hernandez', '/assets/img/team/alejandro.jpg', $mech, '2015', 'email', 'teams', 'Coffee-drinker, email-sender and soldering expert.');
 $noosh_noosh = new Person('Nusha Laleh', $placeholderPhoto, $mech, '2015', 'email', 'teams', 'Professional People Person&#8482;');
-$brian = new Person('Brian Ca$h', $placeholderPhoto, $mech, '2015', 'email', 'teams', 'WHEEEEEEEEEEEEEEEEEEEEEEEEEEEE');
+$brian = new Person('Brian Ca$h', $placeholderPhoto, $mech, '2015', 'email', 'teams', 'Brian helped build some stuff, and provided ample comic relief.');
 $dor = new Person('Dor Gamliel', $placeholderPhoto, $mech, 'class', 'email', 'teams', 'bio');
 
-$micah = new Person('Micah Brouwer', $placeholderPhoto, 'title', '2017', 'email', 'teams', 'bio');
+$micah = new Person('Micah Brouwer', $placeholderPhoto, 'Electrical Engineering', '2017', 'micah.brouwer@utdallas.edu', 'teams', 'Micah helps with the wiring and electrical setup of the Blender. He also edits our videos, and promotes the team.');
 $aaron = new Person('Aaron Plauch&#232;', '/assets/img/team/aaron.png', 'Beard Engineering', '2018', 'jeremiah@utdbattlebots.com', 'leadership, mechanical', $aaron_bio);
 
-$david = new Person('David Merz', $placeholderPhoto, 'Sheep Engineering', '2018', 'david@utdbattlebots.com', 'teams', 'David has a swiss army knife with a hammer. He calls it his pocket hammer. That\'s all you really need to know.');
+$david = new Person('David Merz', '/assets/img/team/david.jpg', $mech, '2018', 'david@utdbattlebots.com', 'teams', 'David works on the mechanical design team. He focuses on dampening and the main structure of the Blender.');
 
-$jeremiah = new Person('Jeremiah Plauch&#232;', $placeholderPhoto, $mech, '2018', 'jeremiah@utdbattlebots.com', 'leadership, mechanical', '"I don\'t know! Things?"');
-$haley = new Person('Haley Abitz', $placeholderPhoto, $mech, '2018', 'haley@utdbattlebots.com', 'leadership, mechanical', 'insert short joke here lolololololol');
-$jack = new Person('Jack Doan', $placeholderPhoto, 'Computer Engineering', 2018, 'jack@utdbattlebots.com', 'leadership, electrical', 'Often seen frantically poking things with a multimeter.');
-$goeckner = new Person('Dr. Matthew Goeckner', '/assets/img/team/goeckner.jpg', 'Mentor', 'n/a', 'goeckner@utdallas.edu', '', 'Chief Safety Officer and plasma physics consultant'); 
+$jeremiah = new Person('Jeremiah Plauch&#232;', '/assets/img/team/jeremiah.jpg', $mech, '2018', 'jeremiah@utdbattlebots.com', 'leadership, mechanical', 'Jeremiah handles the CAD and modeling for the Blender, as well as the mechanical design. He focuses primarily on the drivetrain and dampening. He is also the primary driver during competitions.');
+$haley = new Person('Haley Abitz', '/assets/img/team/haley.jpg', $mech, '2018', 'haley@utdbattlebots.com', 'leadership, mechanical', 'Haley works on the mechanical design for the Blender, makes financial decisions, and works with the university to organize STEM outreach events.');
+$lance = new Person('Lance Gotohio', '/assets/img/team/lance.jpg', 'Computer Engineering', '2019', 'lance@utdbattlebots.com', 'electrical', 'I dont know! Uh, are you typing that?');
+$morganne = new Person('Morganne Blaylock', '/assets/img/team/morganne.jpg', $mech, '2019', 'morganne@utdbattlebots.com', 'social', 'Morganne handles our social media, as well as outreach programs with local schools. ');
+$jack = new Person('Jack Doan', '/assets/img/team/jack.jpg', 'Computer Engineering', 2018, 'jack@utdbattlebots.com', 'leadership, electrical', 'Jack handles most things related to the Blender\'s electronics, including battery management, motors & motor drivers, as well as the control systems.');
+$goeckner = new Person('Dr. Matthew Goeckner', '/assets/img/team/goeckner.jpg', 'Faculty Mentor', 'n/a', 'goeckner@utdallas.edu', '', 'Dr. Goeckner helps us keep a good relationship with the university. He also acts as our Chief Safety Officer and primary plasma physics consultant'); 
 
-$team = array($jack, $haley, $jeremiah, $noosh_noosh, $alejandro, $aaron, $brian, $micah, $david, $dor, $stuart, $alex, $goeckner); // $jeremiah, $haley, 
+$team = array($jack, $haley, $jeremiah, $david, $lance, $morganne, $alejandro, $aaron, $noosh_noosh, $stuart, $alex, $goeckner); // $jeremiah, $haley, 
 $numberOfPeople = count($team);
 $path = "/var/www/utdbattlebots.com/template/";
 
--- template/head.php
+++ template/head.php
@@ -8,6 +8,24 @@
     <title>UTD BattleBots</title>
     <!-- Bootstrap core CSS -->
     <link href="/assets/css/bootstrap.css" rel="stylesheet">
+    <style>
+	.video-container {
+    position: relative;
+    padding-bottom: 56.25%;
+    padding-top: 30px; height: 0; overflow: hidden;
+}
+ 
+.video-container iframe,
+.video-container object,
+.video-container embed {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
+    </style>
     <!-- Bootstrap theme
     <link href="/assets/css/bootstrap-theme.min.css" rel="stylesheet" -->
     <!-- Custom styles for this template 
